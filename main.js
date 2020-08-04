a4db96537976536fc97d19840b45c8e672b84c03
Marco Alzamora Cueto <alzamora1008@gmail.com>
2020-08-04 01:14:27 -0500

    corrigiendo formato

--- main.js
+++ main.js
@@ -17,13 +17,17 @@ Promise.all([
   
   video.addEventListener('play', () => {
     const canvas = faceapi.createCanvasFromMedia(video)
-    document.body.append(canvas)
     const displaySize = { width: video.width, height: video.height }
+
+    document.body.append(canvas)
     faceapi.matchDimensions(canvas, displaySize)
+
     setInterval(async () => {
       const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
       const resizedDetections = faceapi.resizeResults(detections, displaySize)
+
       canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+      
       faceapi.draw.drawDetections(canvas, resizedDetections)
       faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
       faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
